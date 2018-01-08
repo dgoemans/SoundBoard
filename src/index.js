fa9a3121dee4871dcd7838515e80d309c75cbee9
David Goemans <dgoemans@gmail.com>
2018-01-08 21:11:47 +0100

    Template project

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    render () {
+        return (<div></div>);
+    }
+}
+
+render(<App/>, document.getElementById('app'));
